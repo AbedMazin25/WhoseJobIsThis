e6e507da0f1fc51783addaa5075c164c06b8e98d
Abed Matinpour <abm772@mail.usask.ca>
2021-02-27 19:03:40 +0330

    Added model tasks

--- myfirstws/settings.py
+++ myfirstws/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello.apps.HelloConfig'
-    'taskmanager.apps.taskmanagerConfig'
+    'hello.apps.HelloConfig',
+    'taskmanager.apps.TaskmanagerConfig'
 ]
 
 MIDDLEWARE = [
--- taskmanager/models.py
+++ taskmanager/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
-# Create your models here.
+from django.conf import settings
+from hello.models import Post
+class Task(models.Model):
+	title = models.CharField(max_length=200)
+	value = models.IntegerField(default=0)
+	estimated_time = models.DateTimeField('estimated completion date')
+	deadline = models.DateTimeField('deadline')
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	
+	def __str__(self):
+		return self.title
+	
