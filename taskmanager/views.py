68a1493f61d9da90c1b8efcf731b494f92eb705e
Abed Matinpour <abm772@mail.usask.ca>
2021-03-05 21:38:32 +0330

    Modify taskmanager templates hierarchy
    Not it follows a better design and code repetition has been decreased by
    by using html blocks

--- taskmanager/views.py
+++ taskmanager/views.py
@@ -8,7 +8,7 @@ def render_page(request):
 	paginator = Paginator(tasks, 20)
 	page_number = request.GET.get('page')
 	page_obj = paginator.get_page(page_number)
-	return render(request, 'taskmanager/index.html', {'page_obj' : page_obj})
+	return render(request, 'taskmanager/mainpage.html', {'page_obj' : page_obj})
 
 def show_info(request, task_id):
     try:
