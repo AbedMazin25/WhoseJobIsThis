bdfb77e096c022112e22fdf3f379322027ea8952
Abed Matinpour <abm772@mail.usask.ca>
2021-05-05 20:39:23 +0430

    Add login with email feature to the taskmanager app
    * Users can now login using their email.

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
+app_name = 'taskmanager'
+
 urlpatterns = [
-    url(r'^$', views.render_page, name='render_page'),
-    url(r'^tasks/(?P<task_id>[0-9]+)/$', views.show_info, name='show_info')
+    path('', views.render_page, name='render_page'),
+    path('tasks/<int:task_id>/', views.show_info, name='show_info'),
+    path('signin/', views.signin, name='signin'),
+    path('weblogin/<str:email_str>/', views.weblogin, name='weblogin'),
 ]
